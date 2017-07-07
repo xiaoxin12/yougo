f72b93005e40e22d3c45f79147ae4a35ece94def
xiaoxin12 <824447083@qq.com>
2017-07-07 10:48:27 +0800

    '首页头部结束'

--- src/components/Shouye.vue
+++ src/components/Shouye.vue
@@ -1,42 +1,37 @@
 <template>
 <div class="all Shouye">
-	<div id="header">
-		首页
-	</div>	
+  <Shouye_head></Shouye_head>
 	<div id="con">
-		
+
 	</div>
 	<navbar></navbar>
 </div>
 </template>
 
 <script>
-	
-import Navbar  from './Nav.vue'
+
+  import Navbar  from './Nav.vue'
+  import Shouye_head  from './shouye/shouye_head.vue'
 export default {
   name: 'shouye',
   components: {
-       Navbar 
+       Navbar,
+       Shouye_head
   },
   data () {
     return {
-    	
+
     }
   },
   methods :{
-  	
+
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-#header{
-	height: 1.5rem;
-	line-height: 1.5rem;
-	background: #0096FF;
-	position: relative;
-	text-align: center;
-	overflow: hidden;
-}
+  span,a{
+    display:block;
+  }
 </style>
---
+++ src/components/shouye/shouye_head.vue
@@ -0,0 +1,96 @@
+<template>
+    <div id="header">
+      <div class="logo fl"></div>
+      <input class="input fl" type="text" placeholder="百丽集团盛夏折扣季 领卷最多减20元">
+      <div class="mesg fr">
+          <p class="span1"></p>
+          <p class="span2">消息</p>
+      </div>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'shouye_head',
+    components: {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods :{
+
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+  .fl{
+    float:left;
+  }
+  .fr{
+    float: right;
+  }
+  input{
+    border:none;
+  }
+
+  #header{
+    padding:0 5px;
+    height: 1.1rem;
+    line-height: 1.1rem;
+    background: rgba(255,255,200,.1);
+    position: relative;
+    text-align: center;
+    overflow: hidden;
+    .logo{
+      width:1.5rem;
+      height:0.9rem;
+      /*padding:0.1rem;*/
+      margin:0.1rem 0;
+      background:url('../../../src/assets/image/index/logo.png') no-repeat;
+   background-size: cover;
+
+    }
+    input.input{
+      width: 7rem;
+      height:0.9rem;
+      line-height: 0.9rem;
+      margin:0.1rem .2rem;
+      /*margin-bottom: 0;*/
+      padding:0 0.3rem 0 0.7rem;
+      -webkit-border-radius: 3px;
+      -moz-border-radius: 3px;
+      border-radius:3px;
+      background: #fff url('../../../src/assets/image/index/search-gray.png') no-repeat ;
+      background-position: 0.1rem 0.2rem;
+      background-size: 0.5rem;
+    }
+    div.mesg{
+      width:0.8rem;
+      height:1rem;
+      text-align:center;
+      .span1{
+        margin:0 auto;
+        width: 0.6rem;
+        height:0.5rem;
+        background:red;
+        /*padding-top:0.5rem;*/
+        background: url("../../../src/assets/image/index/information-small.png") no-repeat;
+        background-size: cover;
+      }
+      .span2{
+        font-size: 12px;
+        width: 0.8rem;
+        heihgt:0.5rem;
+        margin-top: -5px;
+        color: #fff;
+      }
+    }
+  }
+</style>
